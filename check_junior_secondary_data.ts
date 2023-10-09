5f960e86d8c9bde32d7616f1b012913667b21196
d1dee <mainaderiq@gmail.com>
2023-10-09 07:13:30 +0300

    feat: remove filler functions

--- check_junior_secondary_data.ts
+++ check_junior_secondary_data.ts
@@ -1,93 +1,3 @@
-function _0x138c() {
-    let _0x147167 = [
-        'getMonth',
-        '16984SbwHZn',
-        'attr',
-        'getElementById',
-        '4nsICtp',
-        'content',
-        'prop',
-        'json',
-        '6aXGhrU',
-        '40417OcbGJc',
-        'ready',
-        'setRequestHeader',
-        'innerHTML',
-        'stringify',
-        ', SCHOOL UIC=',
-        'getFullYear',
-        '6992240NekqXp',
-        '1258313WmRMYi',
-        'yyyymmdd',
-        'red',
-        'appendChild',
-        'toString',
-        '1025400lbMixQ',
-        'Assessment Number: ',
-        'val',
-        '#btnUsers2',
-        'GET',
-        'Authorization',
-        'visible',
-        'preventDefault',
-        '416090IGYtea',
-        'AssessmentConfirm',
-        'disabled',
-        'createTextNode',
-        '909Scimbm',
-        '202539NsVQAr',
-        '#AssessmentNumber',
-        '#peoplemessage',
-        'reported',
-        'length',
-        'style.color',
-        'Basic bmVtaXNhZG1pbjo5ODc2JFRldGE=',
-        'click',
-        '.BtnAssCheck',
-        '/generic2/api/Jss/',
-        'meta[name="csrf-token;',
-        'log',
-        'origin',
-        'location'
-    ];
-    _0x138c = function () {
-        return _0x147167;
-    };
-    return _0x138c();
-}
-function _0x9104(_0x5ad0f3, _0x5b77ce) {
-    let _0x138c72 = _0x138c();
-    _0x9104 = function (_0x9104c4, _0x4a4611) {
-        _0x9104c4 = _0x9104c4 - 0x1e7;
-        let _0x2197e1 = _0x138c72[_0x9104c4];
-        return _0x2197e1;
-    };
-    return _0x9104(_0x5ad0f3, _0x5b77ce);
-}
-let _0x24131a = _0x9104;
-(function (_0x9de8bc, _0x2b07dd) {
-    let _101a98c2 = _0x9de8bc();
-    while (true) {
-        try {
-            let _0x454096 =
-                -parseInt(_0x9104(0x205)) / 0x1 +
-                parseInt(_0x9104(0x1e9)) / 0x2 +
-                (-parseInt(_0x9104(0x1ee)) / 101) * (-parseInt(_0x9104(0x200)) / 0x4) +
-                (-parseInt(_0x9104(0x212)) / 0x5) * (-parseInt(_0x9104(0x204)) / 0x6) +
-                parseInt(_0x9104(0x20d)) / 0x7 +
-                (-parseInt(_0x9104(0x1fd)) / 0x8) * (-parseInt(_0x9104(0x1ed)) / 0x9) +
-                -parseInt(_0x9104(0x20c)) / 0xa;
-            if (_0x454096 === _0x2b07dd) {
-                break;
-            } else {
-                _101a98c2.push(_101a98c2.shift());
-            }
-        } catch (_0x15e3f8) {
-            _101a98c2.push(_101a98c2.shift());
-        }
-    }
-})(_0x138c, 0x2100b);
-
 $(function () {
     let origin = window.location.origin;
 
@@ -98,10 +8,10 @@ $(function () {
     });
 
     Date.prototype.yyyymmdd = function () {
-        let _0x1d7679 = this.getFullYear().toString();
-        let _0x29a67a = (this.getMonth() + 0x1).toString();
-        let _0x44c1b4 = this.getDate().toString();
-        return _0x1d7679 + '-' + (_0x29a67a[0x1] ? _0x29a67a : '0' + _0x29a67a[0x0]) + '-' + (_0x44c1b4[0x1] ? _0x44c1b4 : '0' + _0x44c1b4[0x0]);
+        let year = this.getFullYear().toString();
+        let month = (this.getMonth() + 0x1).toString();
+        let day = this.getDate().toString();
+        return year + '-' + (month[0x1] ? month : '0' + month[0x0]) + '-' + (day[0x1] ? day : '0' + day[0x0]);
     };
 
     let assessmentNumber = $('#AssessmentNumber').val();
@@ -149,7 +59,7 @@ $(function () {
             success: function (results) {
                 if (results != null) {
                     let returnMessage = 'Assessment Number: ' + assessmentNumber.toString() + ', ';
-                    
+
                     returnMessage += results.upi.toString() + ', ';
                     returnMessage += results.fullName.toString() + ', SCHOOL UIC=';
                     returnMessage += results.reported.toString();
